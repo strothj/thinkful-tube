d63c644f71f22c743aa25f2fe67636ec2acc1a7c
strothj <jason@jasons.io>
2016-10-05 14:57:27 -0400

    Added ESLint, refactored to resolve linting errors

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
-var API_KEY = "AIzaSyDQ6iEJPREVbNl-z1krbf3eNIjCATu5aBk";
-var ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
+var API_KEY = 'AIzaSyDQ6iEJPREVbNl-z1krbf3eNIjCATu5aBk';
+var ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
 var MAX_RESULTS = 12 * 4;
 var nextPage;
 
-$(function () {
-    $("#js-search-form").submit(startSearch);
-    $(".js-container").on("click", ".js-next-page", getMoreResults);
-});
+function handleListResponse(data) {
+  $('.js-next-page').remove();
 
-function startSearch(event) {
-    event.preventDefault();
-    submitSearch();
-}
+  nextPage = data.nextPageToken || null;
+  var html = '<div class="row">';
+  for (var i = 0; i < data.items.length; i++) {
+    html += '<div class="col-3"><img src="' +
+            data.items[i].snippet.thumbnails.medium.url +
+            '"></div>';
+    if ((i + 1) % 12 === 0) {
+      html += '</div><div class="row">';
+    }
+  }
+  html += '</div>';
 
-function getMoreResults(event) {
-    event.preventDefault();
-    submitSearch(nextPage);
+  if (nextPage) {
+    html += '<div class="row"><div class="col-12"><a href="#" class="next-page js-next-page">More</a></div></div>';
+  }
+
+  $('.js-container').append(html);
+  $('#js-search-form').children().prop('disabled', false);
 }
 
 function submitSearch(page) {
-    $("#js-search-form").children().prop("disabled", true);
-    var q = {
-        key: API_KEY,
-        part: "snippet",
-        q: $("#js-search-query").val(),
-        maxResults: MAX_RESULTS
-    };
-    if (page) { q.pageToken = page; }
-    else { $(".js-container").empty(); }
-    $.getJSON(ENDPOINT, q, handleListResponse);
+  $('#js-search-form').children().prop('disabled', true);
+  var q = {
+    key: API_KEY,
+    part: 'snippet',
+    q: $('#js-search-query').val(),
+    maxResults: MAX_RESULTS
+  };
+  if (page) {
+    q.pageToken = page;
+  } else {
+    $('.js-container').empty();
+  }
+  $.getJSON(ENDPOINT, q, handleListResponse);
 }
 
-function handleListResponse(data) {
-    $(".js-next-page").remove();
-
-    nextPage = data.nextPageToken || null;
-    var html = '<div class="row">';
-    for (var i = 0; i < data.items.length; i++) {
-        html += '<div class="col-3"><img src="' +
-            data.items[i].snippet.thumbnails.medium.url +
-            '"></div>';
-        if ((i + 1) % 12 === 0) {
-            html += '</div><div class="row">';
-        }
-    }
-    html += '</div>';
-
-    if (nextPage) {
-        html += '<div class="row"><div class="col-12"><a href="#" class="next-page js-next-page">More</a></div></div>';
-    }
-
-    $(".js-container").append(html);
-    $("#js-search-form").children().prop("disabled", false);
+function startSearch(event) {
+  event.preventDefault();
+  submitSearch();
 }
+
+function getMoreResults(event) {
+  event.preventDefault();
+  submitSearch(nextPage);
+}
+
+$(function main() {
+  $('#js-search-form').submit(startSearch);
+  $('.js-container').on('click', '.js-next-page', getMoreResults);
+});
